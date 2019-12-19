cf88803fd8482dd59d1aa6c5fb129472a245e591
xialu <2770728609@qq.com>
2019-12-19 21:11:52 +0800

    获取所有最长子序列.

--- src/main/java/com/demo/dynamic/ChildrenStr.java
+++ src/main/java/com/demo/dynamic/ChildrenStr.java
@@ -1,19 +1,29 @@
 package com.demo.dynamic;
 
+
+import java.util.Stack;
+
 import static java.lang.Math.max;
 
+/**
+ * 求字符串的最长子序列.
+ */
 public class ChildrenStr {
 
     private static int[][] arr;
+    private static int[] X;
+    private static int[] Y;
+    private static int num;
 
     public static void main(String[] args) {
-        int[] X = {1, 2, 3, 2, 4, 1, 2, 3, 2, 6, 7, 8, 1, 2, 5, 9, 0, 5};
-        int[] Y = {2, 4, 3, 1, 2, 1, 3, 9, 5, 0};
-        int len = childrenStr(X, 0, X.length, Y, 0, Y.length);
+
+        X = new int[]{1, 2, 3, 2, 4, 1, 2};
+        Y = new int[]{2, 4, 3};
+        int len = childrenStr(0, X.length, 0, Y.length);
         System.out.println(len);
 
         arr = new int[X.length + 1][Y.length + 1];
-        int num = childrenStr(X, X.length, Y, Y.length);
+        num = childrenStr(X.length, Y.length);
         System.out.println(num);
         for (int i = 0; i < X.length; i++) {
             for (int j = 0; j < Y.length; j++) {
@@ -21,21 +31,38 @@ public class ChildrenStr {
             }
             System.out.println();
         }
+//        StringBuilder sb = new StringBuilder();
+        Stack<Integer> sb = new Stack<>();
+        LHS(X.length - 1, Y.length - 1, sb);
     }
 
-    // 最长公共子序列.
-    public static int childrenStr(int[] X, int i, int m, int[] Y, int j, int n) {
+    /**
+     * 最长公共子序列
+     *
+     * @param i 当前子问题.
+     * @param m 字符串长度.
+     * @param j 当前子问题.
+     * @param n 字符串长度.
+     * @return
+     */
+    public static int childrenStr(int i, int m, int j, int n) {
         if (i == m || j == n) {
             return 0;
         } else if (X[i] == Y[j]) {
-            System.out.println(Y[j]);
-            return 1 + childrenStr(X, i + 1, m, Y, j + 1, n);
+            return 1 + childrenStr(i + 1, m, j + 1, n);
         } else {
-            return max(childrenStr(X, i + 1, m, Y, j, n), childrenStr(X, i, m, Y, j + 1, n));
+            return max(childrenStr(i + 1, m, j, n), childrenStr(i, m, j + 1, n));
         }
     }
 
-    private static int childrenStr(int[] X, int m, int[] Y, int n) {
+    /**
+     * 动态规划求解最长子序列长度..
+     *
+     * @param m 字符串长度.
+     * @param n 字符串长度.
+     * @return 最长子序列长度.
+     */
+    private static int childrenStr(int m, int n) {
 
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
@@ -53,4 +80,35 @@ public class ChildrenStr {
         }
         return arr[0][0];
     }
+
+    /**
+     * 获取所有最长子序列.
+     *
+     * @param m 字符串长度.
+     * @param n 字符串长度.
+     */
+    public static void LHS(int m, int n, Stack<Integer> sb) {
+
+        Stack<Integer> stringBuilder = sb;
+        if (num == 0) {
+            while (!sb.isEmpty()) {
+                System.out.println(sb.peek());
+            }
+            sb.pop();
+            return;
+        }
+        if (X[m] == Y[n]) {
+            num--;
+            stringBuilder.push(X[m]);
+            LHS(m - 1, n - 1, stringBuilder);
+        } else if (arr[m - 1][n] > arr[m][n - 1]) {
+            LHS(m - 1, n, stringBuilder);
+        } else if (arr[m][n - 1] > arr[m - 1][n]) {
+            LHS(m, n - 1, stringBuilder);
+        } else {
+            LHS(m - 1, n, stringBuilder);
+            LHS(m, n - 1, stringBuilder);
+        }
+    }
+
 }
